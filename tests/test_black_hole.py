43ad7fa46c87ad47162780007ad684b2bc814231
bjorge@framework <bjorge1997@hotmail.com>
2025-08-26 02:46:15 +0200

    Update tests for new API

--- tests/test_black_hole.py
+++ tests/test_black_hole.py
@@ -13,7 +13,7 @@ def test_varying_mass(mass, incl):
     radii = np.linspace(6*mass, 60*mass, 10)
     bh.calc_isoradials(direct_r=radii, ghost_r=radii)  # calculate some isoradials, should be quick enough
     for isoradial in bh.isoradials:
-        assert not any(np.isnan(isoradial.radii_b)), "Isoradials contain nan values"
+        assert not any(np.isnan(isoradial.impact_parameters)), "Isoradials contain nan values"
         assert not any(np.isnan(isoradial.angles)), "Isoradials contain nan values"
     return None
 
